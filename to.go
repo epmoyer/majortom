90154f6104f4c6c555a6ff427d571b7f541cdd33
Eric Moyer <eric@lemoncrab.com>
2023-01-05 15:19:46 -0800

    read shortcuts

--- to.go
+++ to.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 const APP_NAME = "to"
 const APP_VERSION = "0.0.1b"
+
+type ShortcutsT struct {
+	Locations map[string]string `json:locations`
+}
 
 func main() {
 	optVersion := flag.Bool("version", false,
@@ -25,5 +31,25 @@ func main() {
 		os.Exit(1)
 	}
 
+	shortcuts := loadShortcuts()
+	for shortcut, path := range shortcuts.Locations {
+		fmt.Println(shortcut, path)
+	}
+
 	os.Exit(0)
+}
+
+func loadShortcuts() ShortcutsT {
+	// Open our jsonFile
+	jsonFile, err := os.Open("to_shortcuts.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer jsonFile.Close()
+
+	var shortcuts ShortcutsT
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &shortcuts)
+	return shortcuts
 }
