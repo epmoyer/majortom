9ccdc5ad3a0f7356f312a05b8b006c1aa842764f
Eric Moyer <eric@lemoncrab.com>
2023-01-07 13:50:48 -0800

    fix color escape codes

--- dist/resources/install.sh
+++ dist/resources/install.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-RED="\033[31m"
-YELLOW="\033[33m"
-GREEN="\033[32m"
-ENDCOLOR="\033[0m"
+RED=$'\033[31m'
+YELLOW=$'\033[33m'
+GREEN=$'\033[32m'
+ENDCOLOR=$'\033[0m'
 
 echo "Installing to /usr/local/bin.  You may be prompted for sudo permissions..."
 sudo cp majortom /usr/local/bin
