1ab1858db381fc8424282006ccc8a0f9b8a6eda2
Eric Moyer <eric@lemoncrab.com>
2023-01-07 08:28:20 -0800

    wip, build multi-platform

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-GOARCH=amd64 go build
-
+GOOS=darwin GOARCH=amd64 go build -o dist/darwin.amd64/majortom
+GOOS=linux GOARCH=amd64 go build -o dist/linux.amd64/majortom
--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-GOOS=linux GOARCH=amd64 go build -o majortom.linux.amd64
+GOOS=linux GOARCH=amd64 go build -o dist/linux.amd64/majortom
