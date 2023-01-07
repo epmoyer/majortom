f1eaffb400228b45fead3cceba6182ffefdcac49
Eric Moyer <eric@lemoncrab.com>
2023-01-07 13:01:51 -0800

    wip, installer

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 do_build () {
-    GOOS=$1
-    GOARCH=$2
+    USE_GOOS=$1
+    USE_GOARCH=$2
     TARGET_DIR=$3
 
-    echo "Building for $GOOS:$GOARCH into $TARGET_DIR..."
+    echo "Building for $USE_GOOS:$USE_GOARCH into $TARGET_DIR..."
 
-    go build -o $TARGET_DIR/majortom
+    GOOS=$USE_GOOS GOARCH=$USE_GOARCH go build -o $TARGET_DIR/majortom
     cp dist/resources/install.sh $TARGET_DIR
     cp dist/resources/helper.sh $TARGET_DIR
 }
