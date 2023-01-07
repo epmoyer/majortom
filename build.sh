439f984b5736b803dda15eb003ede2e7c81ec5b9
Eric Moyer <eric@lemoncrab.com>
2023-01-07 08:41:41 -0800

    copy installer to dist dirs

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 GOOS=darwin GOARCH=amd64 go build -o dist/darwin.amd64/majortom
+cp dist/resources/install.sh dist/darwin.amd64/
 GOOS=linux GOARCH=amd64 go build -o dist/linux.amd64/majortom
+cp dist/resources/install.sh dist/linux.amd64/
