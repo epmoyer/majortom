7d39d08de7ba41829f1f90e5b304a667afd0ed38
Eric Moyer <eric@lemoncrab.com>
2023-01-07 12:38:02 -0800

    wip, installer

--- dist/resources/install.sh
+++ dist/resources/install.sh
@@ -1,30 +1,46 @@
 #/bin/bash
-echo "Installing to /usr/local/bin.  You may be prompted for sudo permissions...."
+echo "Installing to /usr/local/bin.  You may be prompted for sudo permissions..."
 sudo cp majortom /usr/local/bin
 sudo chmod 755 /usr/local/bin/majortom
 
+install_shell_function() {
+    echo "         Installing shell function in $FILE..."
+    echo "         (NOT IMPLEMENTED)"
+}
+
+query_install_shell_function() {
+    read -p "      Add to() function to $FILE ? " -n 1 -r
+    echo
+    if [[ ! $REPLY = ^[Yy]$ ]]
+    then
+        install_shell_function $FILE
+    fi
+}
+
 check_shell_script () {
     FILE=$1
-    echo "   looking for existing to() function..."
+    echo "      looking for existing to() function..."
     if grep -Fxq "to () {" $FILE
     then
-        echo "      Found."
+        echo "         Found."
     else
-        echo "      Not found."
+        echo "         Not found."
+        query_install_shell_function $FILE
     fi
 }
 
 process_shell_script () {
     FILE=$1
-    echo "Checking for $FILE..."
+    echo "   Checking for $FILE..."
     if test -f "$FILE"; then
-        echo "   Found."
+        echo "      Found."
         check_shell_script $FILE
     else
-        echo "   Does not exist."
+        echo "      (Does not exist)"
     fi
 }
 
+echo "Adding to() function to shell script..."
 process_shell_script ~/.bashrc
 process_shell_script ~/.zshrc
 process_shell_script ~/dne
