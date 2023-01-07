152083be76bf1ac051262e92a8dbafa79b5d65d0
Eric Moyer <eric@lemoncrab.com>
2023-01-07 15:54:49 -0800

    wip, build images

--- build.sh
+++ build.sh
@@ -3,16 +3,24 @@
 do_build () {
     USE_GOOS=$1
     USE_GOARCH=$2
-    TARGET_DIR=$3
+    TARGET_NAME=$3
+    TARGET_DIR=dist/$3
+    TARGET_ARCHIVE=dist/images/$3.zip
 
     echo "Building for $USE_GOOS:$USE_GOARCH into $TARGET_DIR..."
 
+    # Build executable
     GOOS=$USE_GOOS GOARCH=$USE_GOARCH go build -o $TARGET_DIR/majortom
+
+    # Copy supporting files
     cp dist/resources/install.sh $TARGET_DIR
     cp dist/resources/helper.sh $TARGET_DIR
+
+    # Build release
+    zip -vrj -FS $TARGET_ARCHIVE $TARGET_DIR. -x "*.gitkeep" -x "*.DS_Store"
 }
 
-do_build darwin amd64 dist/darwin.amd64
-do_build linux amd64 dist/linux.amd64
+do_build darwin amd64 macos.amd64
+do_build linux amd64 linux.amd64
 
 echo "Done."
